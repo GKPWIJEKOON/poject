dd50f3d61f7b92e4d5ca487a7320e16cfcd2fb80
Kanishka Wijekoon <kanishkawijekoon2@gmail.com>
2025-05-04 16:47:55 +0530

    data base conneted

--- ELearnXpert/src/main/java/com/zos/ELearnXpert.java
+++ ELearnXpert/src/main/java/com/zos/ELearnXpert.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ELearnXpert {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ELearnXpert.class, args);
 	}
